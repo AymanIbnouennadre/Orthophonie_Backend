0cda2d0e75df2b60121bae5a196d04bcaad1f031
Ayman Ibnouennadre <Ayman.Ibnouennadre@emsi-edu.ma>
2025-03-09 00:05:14 +0000

    upload GTTS API FOR AXIOS REQUEST

--- app/APIs/text_to_speechAR.py
+++ app/APIs/text_to_speechAR.py
@@ -1,29 +1,30 @@
 from gtts import gTTS
 from fastapi import APIRouter, HTTPException
-from fastapi.responses import FileResponse
-import os
+from fastapi.responses import StreamingResponse
+from io import BytesIO
 
 router = APIRouter()
 
-# Spécifier le répertoire de sortie
-output_directory = "outputs"
-os.makedirs(output_directory, exist_ok=True)  # Créer le répertoire s'il n'existe pas
 
-
-@router.post("/synthesize_ar/")
-async def synthesize_text_ar(text: str):
+@router.post("/")
+async def synthesize_text_ar(text: str = None):
+    if not text:
+        raise HTTPException(status_code=400, detail="Text parameter is required")
     try:
         # Générer l'audio à partir du texte en arabe
         tts = gTTS(text=text, lang='ar')
 
-        # Spécifier le chemin du fichier de sortie
-        output_path = os.path.join(output_directory, "output_ar.mp3")
+        # Créer un buffer en mémoire
+        audio_buffer = BytesIO()
+        tts.write_to_fp(audio_buffer)
+        audio_buffer.seek(0)
 
-        # Sauvegarder l'audio dans le fichier
-        tts.save(output_path)
-
-        # Retourner le fichier audio généré
-        return FileResponse(output_path, media_type="audio/mp3")
+        # Retourner le streaming response directement
+        return StreamingResponse(
+            audio_buffer,
+            media_type="audio/mp3",
+            headers={"Content-Disposition": "inline"}
+        )
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error in TTS processing: {str(e)}")
